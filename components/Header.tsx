57aed83a420fb065d670dce784d42dc0f2af498c
romerocam <romerocam@hotmail.com>
2022-11-17 13:54:20 -0300

    header ok

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Vector from "../public/assets/Vector.png";
 import doodle from "../public/assets/doodle.png";
+import avatar from "../public/assets/avatar.png";
+import Link from "next/link";
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#BFD732]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <Image src={Vector} alt="" />
         <Image src={doodle} alt="" />
@@ -15,6 +35,13 @@ const Header = () => {
           <li className="headerLink">Stadistics</li>
           <li className="headerLink">Reports</li>
           <li className="headerLink">Profile</li>
+          {/* <li className="headerLink"><Link href="/account">
+          <Image
+            src={avatar}
+            alt=""
+            className="cursor-pointer rounded-full"
+          />
+        </Link></li> */}
         </ul>
       </div>
     </header>
