afeb3237e2ee404cfb6567277677e799f793f1f5
romerocam <romerocam@hotmail.com>
2022-11-17 12:50:48 -0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,15 @@ const Header = () => {
       <div className="flex items-center space-x-2 md:space-x-10">
         <Image src={Vector} alt="" />
         <Image src={doodle} alt="" />
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-bold">
         <ul className="hidden space-x-4 md:flex">
-            <li>Users</li>
-            <li>Stadistics</li>
-            <li>Reports</li>
-            <li>Profile</li>
+          <li className="headerLink">Users</li>
+          <li className="headerLink">Stadistics</li>
+          <li className="headerLink">Reports</li>
+          <li className="headerLink">Profile</li>
         </ul>
       </div>
-      <div></div>
     </header>
   );
 };
