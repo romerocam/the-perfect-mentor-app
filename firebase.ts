84481a4b1c00b336d1fabd531e2deefe211faacf
romerocam <romerocam@hotmail.com>
2022-11-24 09:31:53 -0300

    fixing log persistence

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAWJqPFJDOmyELvQ5FUCg3bietEvEFJNIM",
     authDomain: "the-perfect-mentor-app.firebaseapp.com",
